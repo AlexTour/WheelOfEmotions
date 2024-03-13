dda953a0edd682d06ae9dcd15c3835fdfdb2a56f
Alexandros Touramanis <32987136+AlexTour@users.noreply.github.com>
2024-03-13 13:52:55 +0100

    Update emotion-onClick.js

--- emotion-onClick.js
+++ emotion-onClick.js
@@ -1,8 +1,17 @@
 import { fetchEmotionDetails } from './emotion-details.js';
 
-// Listen for clicks on hotspots
+const closeButton = document.getElementById('close-button');
 const modelViewer = document.getElementById('modelviewer');
 
+// Function to close the emotion details container
+function closeEmotionDetails() {
+    detailsContainer.classList.remove('show');
+}
+
+// Event listener for the close button click
+closeButton.addEventListener('click', closeEmotionDetails);
+
+// Listen for clicks on hotspots
 modelViewer.querySelectorAll('button').forEach((hotspot) => {
     hotspot.addEventListener('click', () => annotationClicked(hotspot));
   });
@@ -30,5 +39,3 @@ function showEmotionDetails(details) {
     `;
     detailsContainer.classList.add('show');
 }
-
-
